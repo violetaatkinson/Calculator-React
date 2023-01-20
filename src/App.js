c2f29eacefe957097a60cf7ab8a8440e4c458f65
violeta atkinson <violeta.atkinson@gmail.com>
2023-01-20 11:59:38 +0100

    calculator done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App =() => {
+
+    const [result, setResult] = useState('')
+
+    const handleClick = (event) => {
+      setResult(result.concat(event.target.name))
+    }
+
+    const clear = () => {
+        setResult('')
+    }
+
+    const calculate = () => {
+        try {
+          setResult(eval(result).toString());
+        } catch(err){
+          setResult('Error')
+        }
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='calculator'>
+            <div className='screen'>{result ? result : "0"}</div>
+            <br/>
+            <div className='buttons'>
+              <button name= '7'  onClick={handleClick}>7</button>
+              <button name= '8' onClick={handleClick}>8</button>
+              <button name= '9' onClick={handleClick}>9</button>
+              <button name= '+' onClick={handleClick}>+</button>
+              <button name= '4' onClick={handleClick}>4</button>
+              <button name= '5' onClick={handleClick}>5</button>
+              <button name= '6' onClick={handleClick}>6</button>
+              <button name= '-' onClick={handleClick}>-</button>
+              <button name= '1' onClick={handleClick}>1</button>
+              <button name= '2' onClick={handleClick}>2</button>
+              <button name= '3' onClick={handleClick}>3</button>
+              <button name= '*' onClick={handleClick}>*</button>
+              <button name= '0' onClick={handleClick}>0</button>
+              <button name= '.' onClick={handleClick}>.</button>
+              <button name= '/' onClick={handleClick}>/</button>
+              <button onClick={calculate} id="equals">=</button>
+            </div>
+            <br/>
+              <button id="clear" onClick={clear}>CLEAR</button> 
+        </div>
     </div>
   );
 }
